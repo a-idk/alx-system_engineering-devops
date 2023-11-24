dc7c62d64f3e2ce3d47c2df04e90bed50967c3dc
a-idk <idoko_research@outlook.com>
2023-11-24 20:28:41 +0100

    advanced task

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,46 @@
+/* Title: zombie.c */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - function that creates an infinite hanging loop
+ * Return: NADA!
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - program that creates 5 zombie processes
+ * Description: For every zombie process created, 
+ * 				it displays "Zombie process created, PID: ZOMBIE_PID"
+ * 				When the parent process and the zombies are created,
+ * 				the infinite_while funciton is called
+ * Return: 0 on Success! (Always)
+ */
+
+int main(void)
+{
+	/* variables declaration */
+	pid_t zom;  /* zom for zombie process */
+	int index = 0;
+
+	while (index < 5)
+	{
+		zom = fork();
+		if (!zom)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zom)
+		index += 1;
+	}
+	infinite_while();
+	return (0);
+}
