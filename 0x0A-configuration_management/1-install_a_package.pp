1466a00ab6fd04295eb583d0e357cc79d38f08bb
a-idk <idoko_research@outlookcom>
2024-01-23 17:12:42 -0800

    puppet update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 ##!/usr/bin/pup
 # install flask 2.1.0 from pip3 using Puppet
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#package {'flask':
+#  ensure   => '2.1.0',
+#  provider => 'pip'
+#  require
+#}
+
+exec {'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0'
 }
