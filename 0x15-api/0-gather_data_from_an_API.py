3bff75bc4676012f5c239c35aeb8fb7207bf3046
a-idk <idoko_research@outlook.com>
2024-02-20 00:07:16 +0100

    data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Title: Gathering data from an API
+Description: Script that, using this REST API, for a given employee ID,
+            returns information about his/her TODO list progress
+@a_idk scripting
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    # setting the url, parameters and variables as given
+    url = "https://jsonplaceholder.typicode.com/"
+    emp_id = sys.argv[1]
+    us_data = requests.get(url + f"users/{emp_id}").json()
+    todo_lst = requests.get(url + "todos", {"userId": emp_id}).json()
+
+    comp_task = []
+    for idx in todo_lst:
+        if idx.get("completed") is True:
+            comp_task.append(idx.get("title"))
+
+    # printing the first line
+    print(f"Employee {us_data.get('name')} is done with task("
+          f"{len(comp_task)}/{len(todo_lst)})")
+    """
+    print(f"Employee {user.get('name')} is done with
+    task({len(comp_task)}/{len(todo_lst)})")
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+        len(comp_task), len(todo_lst)))
+    """
+
+    # printing the second line
+    for idx in comp_task:
+        print(f"\t{idx}")
---
+++ 0x15-api/READMED.md
@@ -0,0 +1,16 @@
+# 0x15-api
+An application program interface (API) is a set of routines, protocols, and tools for building software applications. Basically, an API specifies how software components should interact. Additionally, APIs are used when programming graphical user interface (GUI) components. A good API makes it easier to develop a program by providing all the building blocks. A programmer then puts the blocks together.
+## Project Description
+Particularly, this project focuses on the following concepts:
+- What Bash scripting should not be used for
+- What is an API
+- What is a REST API
+- What are microservices
+- What is the CSV format
+- What is the JSON format
+- Pythonic Package and module name style
+- Pythonic Class name style
+- Pythonic Variable name style
+- Pythonic Function name style
+- Pythonic Constant name style
+- Significance of CapWords or CamelCase in Python
